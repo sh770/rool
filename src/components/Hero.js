8bd2d40d38881912a5b829ad0e76842ca3678f5d
Shalom Harpaz <sh770h@gmail.com>
2023-07-25 17:48:23 +0300

    2.2

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import photoGrid from '../images/photo_grid.png'
 export default function Hero() {
     return (
         <section className="hero">
-            <img className="hero-photo" src={photoGrid} alt="grid of course lol"/>
+            <img className="hero-photo" src={photoGrid} alt="phot grid of course lol"/>
             <h1 className="hero-header">Why use AirBnB if you have this dope alternative?!</h1>
             <p className="hero-text">All the awesome experiences of people around the world, finally, in one platform wowsuch</p>
         </section>
